78d40d85afe0eb4f61c49e352fa2f1ad87fd416c
Kosta <kosta99dt@gmail.com>
2023-08-09 19:38:33 +0200

    factory and seeders

---
+++ database/seeders/TagsSeeder.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Tag;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+
+class TagsSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     */
+    public static function run(): void
+    {
+        Tag::factory(3)->create();
+    }
+}
